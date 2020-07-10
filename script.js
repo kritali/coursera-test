513bdb587de181895241b9b1f4eefd34d98eb2e5
kritali <67192799+kritali@users.noreply.github.com>
2020-07-10 17:58:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const helloSpeaker = {
+	speakWord: 'Hello',	
+};
+
+
+(function () {
+	helloSpeaker.speak = function (userName) {
+		return (helloSpeaker.speakWord + " " + userName);
+	}
+}());
+
+
+
+const byeSpeaker = {
+	speakWord: 'Good Bye'
+};
+
+(function () {
+	byeSpeaker.speak = function (userName) {
+		return (byeSpeaker.speakWord + " " + userName);
+	}
+}());
+
+
+(function () {
+	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+	for (var i = 0; i < names.length; i++) {
+		var name = names[i];
+		var firstLetter = name.charAt(0);
+		if (firstLetter === 'j'||firstLetter === 'J'){
+			console.log(byeSpeaker.speak(name));
+
+		} else {
+			console.log(helloSpeaker.speak(name));
+			
+		}
+	}
+})();
